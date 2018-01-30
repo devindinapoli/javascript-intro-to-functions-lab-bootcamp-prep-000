380e3f821164f10d2f54b9a64635bd9d9938d17b
Devin DiNapoli <devindinapoli@gmail.com>
2018-01-30 01:26:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
   else if ((string)===lowercase) {
     return "I can't hear you!"
   }
-  else if ((string)==="I love you, Grandma") {
-    return "I love you, too"
+  else if ((string)==="I love you, Grandma.") {
+    return "I love you, too."
   }
 }
